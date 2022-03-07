b751a8080dfa6ba14ab229405caed3704f27b8b2
v.liparteliani <v.liparteliani@adjarabet.com>
2022-03-07 23:10:27 +0400

    change jwt token expire time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1200s' },
+      signOptions: { expiresIn: '600s' },
     }),
   ],
   providers: [AuthService, JwtStrategy],
